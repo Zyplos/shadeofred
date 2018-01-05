77d9a79eaafec548e3f758616bd3739bfa6aa7dc
Zyplos <officialzyplos@gmail.com>
2018-01-04 23:37:10 -0600

    better rgb to hex conversion

--- ff3e3e.js
+++ ff3e3e.js
@@ -1,40 +1,32 @@
-// https://stackoverflow.com/questions/2353211/hsl-to-rgb-color-conversion
-function hslToRgb(h, s, l) {
-    var r, g, b;
-
-    if (s == 0) {
-        r = g = b = l; // achromatic
-    } else {
-        var hue2rgb = function hue2rgb(p, q, t) {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        }
-
-        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
+// https://haacked.com/archive/2009/12/29/convert-rgb-to-hex.aspx/
+function rgbToHex(color) {
+    if (color.substr(0, 1) === '#') {
+        return color;
     }
+    var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
+    console.log(color, digits);
 
-// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
+    var red = parseInt(digits[2]);
+    var green = parseInt(digits[3]);
+    var blue = parseInt(digits[4]);
 
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+    var rgb = blue | (green << 8) | (red << 16);
+    return digits[1] + '#' + rgb.toString(16);
+};
 
 function generateColor() {
+
+    var h_rand = 0;
+    /*
+    if (Math.random() > 0.5) {
+        h_rand = Math.floor(Math.random() * (360 - 355 + 1) + 355);
+
+    } else {
+        h_rand = Math.floor(Math.random() * (5 - 0 + 1) + 5);;
+    }*/
+
+
     var s_rand;
 
     if (Math.random() > 0.5) {
@@ -47,10 +39,11 @@ function generateColor() {
     var l_rand = Math.floor(Math.random() * (70 - 20 + 1) + 20);
 
 
-    console.log(s_rand);
-    console.log(l_rand);
+    console.log("---------");
+    console.log(h_rand, s_rand, l_rand);
+    console.log("---------");
 
-    document.querySelector("#bk_c").style.backgroundColor = "hsla(0, " + s_rand + "%, " + l_rand + "%, 1)";
+    document.querySelector("#bk_c").style.backgroundColor = "hsla(" + h_rand + ", " + s_rand + "%, " + l_rand + "%, 1)";
 
     // just in case
 
@@ -67,9 +60,7 @@ function generateColor() {
     console.log(s_rand_formatted);
     console.log(l_rand_formatted);
 
-    var hslconv = hslToRgb(0, s_rand_formatted, l_rand_formatted);
-
-    var hexValue = rgbToHex(hslconv[0], hslconv[1], hslconv[2]);
+    var hexValue = rgbToHex(bk_c.style.backgroundColor);
 
     document.querySelector("#hexcolor").innerHTML = hexValue;
 
